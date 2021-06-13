823b90cf9b95079874c2ebafe0f912950b2c342c
GilangBrilians <gilangbrilians.f@gmail.com>
2021-06-13 21:40:07 +0700

    added update barang & input barang

---
+++ resources/views/Pages/InputBarang.blade.php
@@ -0,0 +1,39 @@
+@extends('Layout.admin')
+
+@section('title','Input Barang')
+
+@section('container')
+
+<link rel="stylesheet" href="{{ asset('itemlist.css') }}">
+
+<div class="sidebar">
+    <h3 class="my-5 text-center">Admin</h3>
+    <a href="#">Item List</a>
+    <a class="active font-weight-bold" href="#">Input & Update Item</a>
+</div>
+
+<div class="container">
+    <div class="col-sm-12">
+        <h1 class="pt-5 ml-5">Input Barang</h1>
+        <form class="mt-5 ml-5">
+            <div class="form-group">
+                <label for="exampleInputEmail1">Item Name</label>
+                <input type="text" class="form-control" placeholder="Masukan nama barang">
+            </div>
+            <div class="form-group">
+                <label for="exampleInputEmail1">Item Category</label>
+                <input type="text" class="form-control" placeholder="Masukan category barang">
+            </div>
+            <div class="form-group">
+                <label for="exampleInputEmail1">Item Description</label>
+                <input type="text" class="form-control" placeholder="Masukan deskripsi barang">
+            </div>
+            <div class="form-group my-5">           
+                <label class="form-label" for="customFile">Picture Image</label>
+                <input type="file" id="customFile" />
+            </div>
+            
+            <button type="submit" class="btn font-weight-bold mt-5"  style="color:white; background-color:#193948; padding: 1% 46% 1% 46%" >Submit</button>
+        </form>
+    </div>
+</div>
