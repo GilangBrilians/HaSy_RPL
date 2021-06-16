3f1a88525e48c44ec80fc0a6a1aa47ac24282a71
dennisfadillah <dennisf@student.telkomuniversity.ac.id>
2021-06-16 18:43:29 +0700

    added crud barang

---
+++ app/Http/Controllers/BarangController.php
@@ -0,0 +1,109 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Barang;
+
+class BarangController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $barang = Barang::all();
+
+        return view('Pages.ItemList', compact('barang'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('Pages.InputBarang');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $data = $request->validate([
+          'nama' => 'required|max:50',
+          'kategori' => 'required|max:100',
+          'deskripsi' => 'required|max:255',
+          'harga' => 'required|numeric',
+        ]);
+
+        $show = Barang::create($data);
+
+        return redirect('/barang')->with('success', 'Barang berhasil di-input');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        $barang = Barang::findOrFail($id);
+
+        return view('Pages.UpdateBarang', compact('barang'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $data = $request->validate([
+          'nama' => 'required|max:50',
+          'kategori' => 'required|max:100',
+          'deskripsi' => 'required|max:255',
+          'harga' => 'required|numeric',
+        ]);
+        Barang::whereId($id)->update($data);
+
+        return redirect('/barang')->with('success', 'Data barang berhasil diubah');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $barang = Barang::findOrFail($id);
+        $barang->delete();
+
+        return redirect('/barang')->with('success', 'Data barang berhasil dihapus');
+    }
+}
--- resources/views/Pages/ItemList.blade.php
+++ resources/views/Pages/ItemList.blade.php
@@ -10,12 +10,17 @@
 
 <div class="sidebar">
     <h3 class="my-5 text-center">Admin</h3>
-    <a class="active font-weight-bold" href="#">Item List</a>
-    <a href="#">Input & Update Item</a>
+    <a class="active font-weight-bold" href="{{ route('barang.index') }}">Item List</a>
+    <a href="{{ route('barang.create') }}">Input Item</a>
 
 </div>
 
     <div class="container">
+      @if(session()->get('success'))
+        <div class="alert alert-success">
+          {{ session()->get('success') }}
+        </div><br />
+      @endif
       <div class="content">
         <h3 class="mt-3 font-weight-bold">Item List</h3>
         <table class="table mt-3">
@@ -23,26 +28,34 @@
             <tr>
               <th scope="col">Item Name</th>
               <th scope="col">Item Category</th>
+              <th scope="col">Item Description</th>
+              <th scope="col">Item Price</th>
               <th scope="col">Action</th>
             </tr>
           </thead>
           <!-- query untuk menampilkan data produk ke interface dari database -->
           <!-- menggunakan looping while sampai semua data ditampilkan-->
           <tbody>
+            @foreach($barang as $bar)
             <tr>
-              <td>Nama</td> <!-- menampilkan nama barang -->
-                <td>Harga</td> <!-- menampilkan harga barang -->
-                <td>
-                  <button class="btn btn-primary"> <!-- button untuk update data barang -->
-                    Update
-                  </button>
+              <td>{{$bar->nama}}</td> <!-- menampilkan nama barang -->
+              <td>{{$bar->kategori}}</td> <!-- menampilkan kategori barang -->
+              <td>{{$bar->deskripsi}}</td> <!-- menampilkan deskripsi barang -->
+              <td>{{$bar->harga}}</td> <!-- menampilkan harga barang -->
+              <td>
+                <a href="{{ route('barang.edit', $bar->id) }}" class="btn btn-primary"> <!-- button untuk update data barang -->
+                  Update
+                </a>
+                <form action="{{ route('barang.destroy', $bar->id) }}" method="POST">
+                  @csrf
+                  @method('DELETE')
                   <button class="btn btn-danger"> <!-- button untuk delete data barang -->
                     Delete
                   </button>
+                </form>
               </td>
             </tr>
-           
-            
+            @endforeach
           </tbody>
         </table>
       </div>
